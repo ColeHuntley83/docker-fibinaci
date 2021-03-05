29c2ba9bb49356ad06bdcf43327f07e69ac3ee6e
ColeHuntley83 <calrugby1@gmail.com>
2021-03-05 10:05:21 -0800

    init process

---
+++ server/keys.js
@@ -0,0 +1,17 @@
+const redisHost = process.env.REDIS_HOST;
+const redisPort = process.env.REDIS_PORT;
+const pgUser = process.env.PGUSER;
+const pgHost = process.env.PGHOST;
+const pgDatabase = process.env.PGDATABASE;
+const pgPassword = process.env.PGPASSWORD;
+const pgPort = process.env.PGPORT;
+
+module.exports = {
+    redisHost,
+    redisPort,
+    pgUser,
+    pgHost,
+    pgDatabase,
+    pgPassword,
+    pgPort
+}
---
+++ worker/keys.js
@@ -0,0 +1,8 @@
+const redisHost = process.env.REDIS_HOST;
+const redisPort = process.env.REDIS_PORT;
+
+
+module.exports = {
+    redisHost,
+    redisPort
+}
